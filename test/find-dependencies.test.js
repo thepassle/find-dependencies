8858a12293474de395376b3aea623681f7001251
thepassle <pascalschilp@gmail.com>
2021-10-17 15:41:05 +0200

    test

--- test/find-dependencies.test.js
+++ test/find-dependencies.test.js
@@ -8,22 +8,22 @@ describe('find-dependencies', ({it}) => {
   it('finds dependencies for monorepo setup', async () => {
     const globs = await globby(['fixtures/monorepo/packages/my-package/*.js']);
     let dependencies = await findDependencies(globs, { basePath: 'fixtures/monorepo/packages/my-package' });
-    dependencies = dependencies.map(d => d.split('find-dependencies')[1]);
+    dependencies = dependencies.map(d => d.split('fixtures')[1]);
 
     assert.deepEqual(dependencies,
       [
-        '/fixtures/monorepo/packages/my-package/node_modules/@scoped/package/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/@scoped/package/baz/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/export-map/long/path/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/nested/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/regular/index.js',
-        '/fixtures/monorepo/node_modules/monorepo-dep/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/internal-shared/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/dynamic-import/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/nested/node_modules/regular/index.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/internal-shared/a.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/internal-shared/b.js',
-        '/fixtures/monorepo/packages/my-package/node_modules/internal-shared/c.js'
+        '/monorepo/packages/my-package/node_modules/@scoped/package/index.js',
+        '/monorepo/packages/my-package/node_modules/@scoped/package/baz/index.js',
+        '/monorepo/packages/my-package/node_modules/export-map/long/path/index.js',
+        '/monorepo/packages/my-package/node_modules/nested/index.js',
+        '/monorepo/packages/my-package/node_modules/regular/index.js',
+        '/monorepo/node_modules/monorepo-dep/index.js',
+        '/monorepo/packages/my-package/node_modules/internal-shared/index.js',
+        '/monorepo/packages/my-package/node_modules/dynamic-import/index.js',
+        '/monorepo/packages/my-package/node_modules/nested/node_modules/regular/index.js',
+        '/monorepo/packages/my-package/node_modules/internal-shared/a.js',
+        '/monorepo/packages/my-package/node_modules/internal-shared/b.js',
+        '/monorepo/packages/my-package/node_modules/internal-shared/c.js'
       ]
     )
   });
@@ -31,12 +31,12 @@ describe('find-dependencies', ({it}) => {
   it('finds dependencies for regular setup', async () => {
     const globs = await globby(['fixtures/regular/*.js']);
     let dependencies = await findDependencies(globs, { basePath: 'fixtures/regular' });
-    dependencies = dependencies.map(d => d.split('find-dependencies')[1]);
+    dependencies = dependencies.map(d => d.split('fixtures')[1]);
 
     assert.deepEqual(dependencies,
       [ 
-        '/fixtures/regular/node_modules/bar/index.js',
-        '/fixtures/regular/node_modules/foo/index.js'
+        '/regular/node_modules/bar/index.js',
+        '/regular/node_modules/foo/index.js'
       ]
     )
   });
